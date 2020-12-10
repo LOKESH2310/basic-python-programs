7875bc3d9a5f7fe023425e8250a24fd475301c91
LOKESH-LOKI-23 <73383730+LOKESH-LOKI-23@users.noreply.github.com>
2020-12-10 21:29:58 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,15 @@
+number = int(input("Enter the number : "))
+check = number
+sum = 0
+reverse = 0
+while check >0 :
+    numbers = check % 10
+    sum = sum + numbers
+    check = check // 10
+    reverse = reverse*10 + numbers
+if reverse == number :
+    print ("It is a palindrome number")
+else :
+    print ("It is not a palindrome number")
+print("Sum of the number : " ,sum)
+print("Reverse of the number : " ,reverse)
