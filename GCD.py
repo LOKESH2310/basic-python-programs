2a8a78c1fd6c8d1f7d02d78a413fd5aaf36a778e
LOKESH-LOKI-23 <73383730+LOKESH-LOKI-23@users.noreply.github.com>
2020-12-16 09:06:42 +0530

    Delete GCD.py

--- GCD.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter the 1st number : "))
-num2 = int(input("Enter the 2nd number : "))
-
-def get_gcd(num1 , num2):
-    while num2 :
-        num1 , num2 = num2 , num1 % num2
-    return num1
-
-print("GCD of two numbers :" ,get_gcd(num1 , num2))
